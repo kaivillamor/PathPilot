1d5d29d1315043263b83380f8231aeaf8a7b7874
kaivillamor <kai.mvillamor@gmail.com>
2026-04-21 17:34:12 -0400

    frontend, backend: linked up to api, removed unecessary buttons, and tested

--- pathpilot-frontend/app.py
+++ pathpilot-frontend/app.py
@@ -18,4 +18,4 @@ def results():
         return render_template("results.html", error=str(e))
     return render_template("results.html", federal_jobs=data["federal_jobs"], private_jobs=data["private_jobs"], degree=degree)
 if __name__ == "__main__":
-    app.run(debug=True, host = "0.0.0.0", port = 5003)
+    app.run(debug=True, host = "0.0.0.0", port = 5000)
